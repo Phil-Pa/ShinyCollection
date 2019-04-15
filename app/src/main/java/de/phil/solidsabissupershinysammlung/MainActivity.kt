027fdc8db6c103661d004bc1d2c3a057edacf9af
Phil-Pa <philipparold.pa@gmail.com>
2019-04-15 19:26:17 +0200

    added some ui

--- app/src/main/java/de/phil/solidsabissupershinysammlung/MainActivity.kt
+++ app/src/main/java/de/phil/solidsabissupershinysammlung/MainActivity.kt
@@ -1,10 +1,12 @@
 package de.phil.solidsabissupershinysammlung
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,9 +22,8 @@ class MainActivity : AppCompatActivity(), PokemonDataFragment.OnListFragmentInte
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener {
+            startActivity(Intent(applicationContext, AddNewPokemon::class.java))
         }
 
     }
@@ -43,7 +44,19 @@ class MainActivity : AppCompatActivity(), PokemonDataFragment.OnListFragmentInte
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.show_average_eggs -> {
+
+                val pokemons = App.getAllPokemon()
+                if (pokemons != null && pokemons.isNotEmpty()) {
+                    val sum = pokemons.sumBy { it.eggsNeeded }
+                    val averageEggs = sum.toDouble() / pokemons.size.toDouble()
+                    Toast.makeText(applicationContext, averageEggs.toString(), Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(applicationContext, "Du hast noch keine Shiny Pokemon!", Toast.LENGTH_SHORT).show()
+                }
+
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
